85ab3847f2544bf74019b525c068153e900dcb3b
sharan93 <spyrotrunks@gmail.com>
2025-03-20 21:49:11 +0530

    port changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
     res.send("Hello, Jenkins-Tomcat Deployment!");
 });
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
