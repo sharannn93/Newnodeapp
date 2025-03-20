84f486f2ed107a0eb0763ef7de1bf98e79074032
sharan93 <spyrotrunks@gmail.com>
2025-03-20 21:22:30 +0530

    New App created

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, Jenkins-Tomcat Deployment!");
+});
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
